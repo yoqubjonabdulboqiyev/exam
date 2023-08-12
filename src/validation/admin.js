825bda461069c7ff99b5672a2268b0186365b144
yoqubjonabdulboqiyev <yoqubjonabdulboqiyev@gmail.com>
2023-08-12 14:39:34 +0500

    update password change

--- src/validation/admin.js
+++ src/validation/admin.js
@@ -7,7 +7,7 @@ const registerV = (data) => {
         firstName: Joi.string().required(),
         lastName: Joi.string(),
         phoneNumber: Joi.string().regex(phoneNumberRegex).required(),
-        password: Joi.string().required()
+        password: Joi.string().min(6).max(20).required()
 
     })
     const { error } = schema.validate({ firstName, lastName, phoneNumber, password })
@@ -23,7 +23,7 @@ const loginV = (data) => {
     const phoneNumberRegex = /^998(9[012345789]|6[125679]|7[01234569])[0-9]{7}$/;
     const schema = Joi.object({
         phoneNumber: Joi.string().regex(phoneNumberRegex).required(),
-        password: Joi.string().required(),
+        password: Joi.string().min(6).max(20).required(),
     })
     const { error } = schema.validate({ phoneNumber, password })
     if (error) {
